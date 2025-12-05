26fa5c0a13db3079d682e598c3d8e994d34e2151
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2025-12-05 08:00:54 +0100

    Update asset paths in configuration and styles for PixelGame
    - Changed sprite URLs in menace.config.ts to point to the correct public directory.
    - Updated villagers' image URLs in villagers.config.ts to reflect new asset paths.
    - Adjusted background image path in village.css to ensure proper loading.
    - Modified index.html to include updated JavaScript and CSS files.

--- src/PixelGame/config/menace.config.ts
+++ src/PixelGame/config/menace.config.ts
@@ -25,7 +25,7 @@ export const THREATS: Entity[] = [
         counterType: "protection-données",
       },
     ],
-    spriteUrl: "/assets/pixelgame/menace1.gif",
+    spriteUrl: "../../../public/assets/pixelgame/menage1.gif",
   },
 
   {
@@ -52,7 +52,7 @@ export const THREATS: Entity[] = [
         counterType: "hébergement-local",
       },
     ],
-    spriteUrl: "assets/pixelgame/menace2.gif",
+    spriteUrl: "../../../public/assets/pixelgame/menace2.gif",
   },
 
   {
@@ -81,7 +81,7 @@ export const THREATS: Entity[] = [
         counterType: "linux",
       },
     ],
-    spriteUrl: "assets/pixelgame/menace3.gif",
+    spriteUrl: "../../../public/assets/pixelgame/menace3.gif",
   },
 
   {
@@ -108,7 +108,7 @@ export const THREATS: Entity[] = [
         counterType: "souveraineté",
       },
     ],
-    spriteUrl: "assets/pixelgame/menace1.gif",
+    spriteUrl: "../../../public/assets/pixelgame/menace1.gif",
   },
 
   {
@@ -137,6 +137,6 @@ export const THREATS: Entity[] = [
         counterType: "réparabilité",
       },
     ],
-    spriteUrl: "assets/pixelgame/menaces/menace1.gif",
+    spriteUrl: "../../../public/assets/pixelgame/menaces/menace1.gif",
   },
 ];
