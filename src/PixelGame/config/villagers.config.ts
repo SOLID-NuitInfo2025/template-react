70d88b64cd01417b12ab9b020c2971d0341e82e3
Julie Fromageat <julie.fr6799@gmail.com>
2025-12-05 07:23:56 +0100

    text villagois

--- src/PixelGame/config/villagers.config.ts
+++ src/PixelGame/config/villagers.config.ts
@@ -1,4 +1,4 @@
-import type { Entity } from "../types/ENtity";
+import type { Entity } from "../types/Entity";
 import { THREATS } from "./menace.config";
 
 export interface VillagerInfo {
@@ -19,7 +19,7 @@ export const villagersConfig: VillagerInfo[] = [
     id: 1,
     name: "Ariane",
     position: { xPercent: 70, yPercent: 25 },
-    intro: "Bienvenue au village ! Je veille sur la place centrale.",
+    intro: "Moi j'aime bien windows, c'est mon ami de tout les jours",
     imageUrl: "/public/assets/pixelgame/villagers/gaulois1.png",
     hoverImageUrl: "/public/assets/pixelgame/villagers/gaulois1_hover.png",
     menace: THREATS[0],
@@ -30,7 +30,7 @@ export const villagersConfig: VillagerInfo[] = [
     id: 2,
     name: "Borin",
     position: { xPercent: 31, yPercent: 48 },
-    intro: "Forgeron du village, je répare vos équipements.",
+    intro: "Je stock tout en ligne pour y accéder facilement depuis partout",
     imageUrl: "/public/assets/pixelgame/villagers/gaulois2.png",
     hoverImageUrl: "/public/assets/pixelgame/villagers/gaulois2_hover.png",
     menace: THREATS[1],
@@ -41,7 +41,7 @@ export const villagersConfig: VillagerInfo[] = [
     id: 3,
     name: "Céleste",
     position: { xPercent: 40, yPercent: 80 },
-    intro: "Herboriste, j'ai toujours quelques remèdes à partager.",
+    intro: "J'aime bien Iphone",
     imageUrl: "/public/assets/pixelgame/villagers/gaulois3.png",
     hoverImageUrl: "/public/assets/pixelgame/villagers/gaulois3_hover.png",
     menace: THREATS[2],
@@ -52,7 +52,7 @@ export const villagersConfig: VillagerInfo[] = [
     id: 4,
     name: "Darius",
     position: { xPercent: 70, yPercent: 68 },
-    intro: "Chasseur aguerri, je protège le village des menaces.",
+    intro: "J'ai des pubs tout les jours",
     imageUrl: "/public/assets/pixelgame/villagers/gaulois4.png",
     hoverImageUrl: "/public/assets/pixelgame/villagers/gaulois4_hover.png",
     menace: THREATS[3],
